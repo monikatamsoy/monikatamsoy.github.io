9be1f9293772068167899c2035d7e91d994839a5
monikatamsoy <tamsoymonika@gmail.com>
2018-03-27 00:15:10 +0530

    add styling

--- src/App.js
+++ src/App.js
@@ -1,53 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-// import './App.css';
-
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router'
-import ExampleForm1 from './components/form'
+import Form from './components/form'
 import Blog from './components/blog'
-// import createHistory from 'history/createBrowserHistory'
-// console.log(window);
+
 class App extends Component {
   render() {
-    if (window.location.pathname=="/") {
-      return(
+    switch (window.location.pathname) {
+      case "/":
+      return (<div className="app">
 
+        <Form/>
 
-        <div className="app">
-
-            <ExampleForm1/>
-
-          {/* <Blog /> */}
-          {/* <Switch>
-          <Route exact path='/' component={ExampleForm1} />
-          <Route exact path='/blog' component={Blog} />
-          </Switch> */}
-
-        </div>
-
-
-
-      );
-    } else if(window.location.pathname=='/blog'){
-      return(
-
-
-        <div className="app">
-<h1>{window.localStorage.blog}</h1>
-            <Blog/>
-
-          {/* <Blog /> */}
-          {/* <Switch>
-          <Route exact path='/' component={ExampleForm1} />
-          <Route exact path='/blog' component={Blog} />
-          </Switch> */}
-
-        </div>
-
-
-
-      );
+      </div>);
+        break;
+      case "/blog":
+        return (<div className="app">
+          {/* <h1>{window.localStorage.blog}</h1> */}
+          <Blog/>
+        </div>);
+        break;
+      default:
+        return 'error'
     }
+    // if (window.location.pathname == "/") {
+    //   return (<div className="app">
+    //
+    //     <ExampleForm1/>
+    //
+    //   </div>);
+    // } else if (window.location.pathname == '/blog') {
+    //   return (<div className="app">
+    //     {/* <h1>{window.localStorage.blog}</h1> */}
+    //     <Blog/>
+    //   </div>);
+    // }
 
   }
 }
--- src/components/form.js
+++ src/components/form.js
@@ -1,89 +1,56 @@
-import React, { Component } from 'react';
-// import {browserHistory, Link,
-// withRouter} from 'react-router';
-// import { browserHistory } from 'react-router'
-// import {
-//   Link
-// } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React, {Component} from 'react';
 import Blog from './blog'
-class ExampleForm1 extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   // this.state = {value: ''};
-  //
-  //   this.handleChange = this.handleChange.bind(this);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  // }
-  state ={
-    errors:false
+
+class Form extends React.Component {
+
+  state = {
+    errors: false
   }
-
-  // handleChange(event) {
-  //   this.setState({value: event.target.value});
-  // }
-
   handleSubmit = (event) => {
     event.preventDefault()
-    let username= event.target[0].value;
-    let password= event.target[1].value;
-
-    // const value= event.target.element.value
-    // const error= this.props.getErrorMessage(value)
-    if (username && password){
-      window.localStorage.user = username
+    let username = event.target[0].value;
+    let password = event.target[1].value;
+// checking for non-empty values
+    if (username && password) {
+      // window.localStorage.user = username
       console.log('proceed to /blog');
       this.setState({errors: false});
-     //  const path = '/blog'
-     // browserHistory.push(path)
-      // this.setState({value: ''});''
       window.location = '/blog'
-    }
-
-    else{
+    } else {
       console.log('show error');
       this.setState({errors: true});
     }
-     // this.props.router.push('/blog');
-    // browserHistory.push('/blog');
     event.preventDefault();
-    // else{
-    //   alert('success: ${value}')
-    // }
+
   }
 
   render() {
     const {errors} = this.state;
     console.log("state", this.state);
 
-    return (
-      // const errorfield= this.state;
-      <div align="center">
+    return (<div align="center">
       <form onSubmit={this.handleSubmit}>
 
         <label htmlFor="username">
           Name:
-          <input name="username" type="text"   />
+          <input name="username" type="text"/>
         </label>
         <label htmlFor="password">
           Password:
-          <input name="password" type="text"   />
+          <input name="password" type="text"/>
         </label>
-        { errors &&
-          <div style={{color: 'red'}}>
-            Incorrect Credentials
-          </div>
+        {
+          errors && <div style={{
+                color: 'red'
+              }}>
+              Incorrect Credentials
+            </div>
         }
         <input type="submit" value="Submit"/>
-
-          {/* <Link to="/blog">Submit</Link> */}
-
-
       </form>
-      </div>
-    );
+    </div>);
   }
 
 }
 
-export default ExampleForm1;
+export default Form;
